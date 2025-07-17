5f59726d73d18d3eb8efd3f740a2ea377d0bffa1
Istiaque <istiaquechowdhury430@gmail.com>
2025-07-17 12:10:54 +0600

    added values controller

---
+++ HRM.WEB/Controllers/ValuesController.cs
@@ -0,0 +1,24 @@
+﻿using HRM.WEB.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HRM.WEB.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+
+        public ValuesController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+
+        public IActionResult Get()
+        {
+            return Ok();
+        }
+    }
+}
